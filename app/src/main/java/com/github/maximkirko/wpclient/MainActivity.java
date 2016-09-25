8aba20c9350e8f4d1e5398b420d80c3fa0622597
MaximKirko <30max30max@gmail.com>
2016-09-25 14:17:15 +0300

    camera activity added

--- app/src/main/java/com/github/maximkirko/wpclient/MainActivity.java
+++ app/src/main/java/com/github/maximkirko/wpclient/MainActivity.java
@@ -3,10 +3,14 @@ package com.github.maximkirko.wpclient;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -14,16 +18,19 @@ public class MainActivity extends AppCompatActivity {
 
     //view components
     private Button btViolPhoto;
-    private Button btNumPhoto;
+    private Button btLPPhoto;
     private ImageView violPhoto;
-    private ImageView numPhoto;
+    private ImageView lpPhoto;
 
     //popups
     private AlertDialog.Builder ad;
 
     //objects for load photos from gallery
-    private static final int GALLERY_REQUEST = 1;
+    private final int GALLERY_REQUEST = 1;
     private Uri cache;
+
+    //objects for make photo with camera
+    private final int CAMERA_RESULT = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +38,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btViolPhoto = (Button) findViewById(R.id.buttonViolPhoto);
-        btNumPhoto = (Button) findViewById(R.id.buttonNumPhoto);
+        btLPPhoto = (Button) findViewById(R.id.buttonLPPhoto);
         violPhoto = (ImageView) findViewById(R.id.imageView);
-        numPhoto = (ImageView) findViewById(R.id.imageView2);
+        lpPhoto = (ImageView) findViewById(R.id.imageView2);
 
         ad = new AlertDialog.Builder(MainActivity.this);
         ad.setTitle(R.string.dialog_photo_title);
         ad.setPositiveButton(R.string.dialog_photo_choice_1, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int arg1) {
-
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_RESULT);
             }
         });
         ad.setNegativeButton(R.string.dialog_photo_choice_2, new DialogInterface.OnClickListener() {
@@ -50,12 +58,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         ad.setCancelable(true);
+
+        //!!! back button pressed
+        ad.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+            }
+        });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-        cache = imageReturnedIntent.getData();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_RESULT) {
+            cache = data.getData();
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+            cache = data.getData();
+        }
     }
 
     public void onClickViolPhoto(View view) {
@@ -66,10 +85,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void onClickNumPhoto(View view) {
+    public void onClickLPPhoto(View view) {
         ad.show();
         if(cache != null) {
-            numPhoto.setImageURI(cache);
+            lpPhoto.setImageURI(cache);
             cache = null;
         }
     }
